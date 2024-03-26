33caefd97679dfb6fa3f0b1c8bcc1b8800a7bee0
Matheus Silva <silva.matheus.dsn@gmail.com>
2024-03-26 01:31:03 -0300

    js btn

--- pages/home/main.js
+++ pages/home/main.js
@@ -7,3 +7,21 @@ function clickMenu() {
         btn__humb.setAttribute("src", "./assets/icons-nav/icon-close.svg")
     }
 }
+
+function right__click() {
+    if (btn__left.style.display == 'block') {
+        btn__left.style.display = 'none'
+    } else {
+        btn__left.style.display = 'block'
+        btn__right.style.display = 'none'
+    }
+}
+
+function left__click() {
+    if (btn__right.style.display == 'block') {
+        btn__right.style.display = 'none'
+    } else {
+        btn__right.style.display = 'block'
+        btn__left.style.display = 'none'
+    }
+}
